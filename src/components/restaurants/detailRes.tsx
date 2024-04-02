4a8c31b9a15b028231e992f8822ee292de80c4b9
NhatNguyen0919 <nhatnha20@uef.edu.vn>
2024-04-02 10:03:11 +0700

    final

--- src/components/restaurants/detailRes.tsx
+++ src/components/restaurants/detailRes.tsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { Image } from "antd";
+import { Image, Layout } from "antd";
+import { Content } from "antd/es/layout/layout";
 
 const DetailRes = (props: any) => {
   const { id }: { id: string } = useParams();
@@ -37,23 +38,58 @@ const DetailRes = (props: any) => {
   return (
     <>
       <div>
-        <Image width={"200px"} src={detailData.image} />
-        <hr />
-        <span>Name : {detailData.name}</span>
-        <hr />
-        <span>Address: {detailData.address}</span>
-        <hr />
-        <span>Phone:{detailData.phone}</span>
-        <hr />
-        <span>Description : {detailData.description}</span>
-        <iframe
-          src={detailData.district}
-          width="600"
-          height="450"
-          allowfullscreen=""
-          loading="lazy"
-          referrerpolicy="no-referrer-when-downgrade"
-        ></iframe>
+        <Layout style={{ backgroundColor: "white" }}>
+          <Content style={{ padding: "40px 48px" }}>
+            <h1
+              style={{
+                textAlign: "center",
+                marginBottom: "20px",
+                fontWeight: "bold",
+                fontSize: "50px",
+              }}
+            >
+              Detail Restaurant
+            </h1>
+            <div style={{ display: "flex", justifyContent: "center" }}>
+              <div style={{ marginRight: "50px" }}>
+                <Image
+                  width={"350px"}
+                  src={detailData.image}
+                  style={{
+                    borderRadius: "5px",
+                    boxShadow: "0px 2px 2px black",
+                  }}
+                />
+              </div>
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  gap: "5px",
+                }}
+              >
+                <span style={{ fontWeight: "bold", fontSize: "24px" }}>
+                  {detailData.name}
+                </span>
+                <span>Address: {detailData.address}</span>
+                <hr />
+                <span>Phone: {detailData.phone}</span>
+                <hr />
+                <span style={{ marginBottom: "20px" }}>
+                  Description : {detailData.description}
+                </span>
+                <iframe
+                  src={detailData.district}
+                  width="350"
+                  height="350"
+                  allowfullscreen=""
+                  loading="lazy"
+                  referrerpolicy="no-referrer-when-downgrade"
+                ></iframe>
+              </div>
+            </div>
+          </Content>
+        </Layout>
       </div>
     </>
   );
